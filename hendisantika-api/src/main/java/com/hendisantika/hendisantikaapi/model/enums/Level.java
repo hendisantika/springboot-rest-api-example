975c2612553cd25dc945262d65b58c6efd075e8e
hendisantika <hendisantika@yahoo.co.id>
2019-03-19 07:15:13 +0700

    #10 - Add Level Enum Model in hendisantika-api module

---
+++ hendisantika-api/src/main/java/com/hendisantika/hendisantikaapi/model/enums/Level.java
@@ -0,0 +1,16 @@
+package com.hendisantika.hendisantikaapi.model.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rest-api-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-03-19
+ * Time: 07:14
+ */
+public enum Level {
+    BEGINNER,
+    ADVANCED,
+    PROFESSIONAL
+}
