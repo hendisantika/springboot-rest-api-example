63e52ac7780a97f37461d2123fa71079d336892d
hendisantika <hendisantika@yahoo.co.id>
2019-03-19 07:16:05 +0700

    #12 - Add Gender Enum Model in hendisantika-api module

---
+++ hendisantika-api/src/main/java/com/hendisantika/hendisantikaapi/model/enums/Gender.java
@@ -0,0 +1,15 @@
+package com.hendisantika.hendisantikaapi.model.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rest-api-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-03-19
+ * Time: 07:15
+ */
+public enum Gender {
+    MALE,
+    FEMALE,
+}
