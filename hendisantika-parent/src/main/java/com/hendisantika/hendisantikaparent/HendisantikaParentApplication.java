0af568e5522540561c5732d24a1b4b512252be35
hendisantika <hendisantika@yahoo.co.id>
2019-03-19 07:05:15 +0700

    #2 - Add hendisantika-parent module

---
+++ hendisantika-parent/src/main/java/com/hendisantika/hendisantikaparent/HendisantikaParentApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.hendisantikaparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HendisantikaParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HendisantikaParentApplication.class, args);
+    }
+
+}
