ada75284f4fbc486d9680053265e1bd975832c5e
hendisantika <hendisantika@yahoo.co.id>
2019-03-19 07:07:39 +0700

    #4 - Add hendisantika-api module

---
+++ hendisantika-api/src/main/java/com/hendisantika/hendisantikaapi/HendisantikaApiApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.hendisantikaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HendisantikaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HendisantikaApiApplication.class, args);
+    }
+
+}
