c4380c2256122c399af0b0ad260bf818290510b3
hendisantika <hendisantika@yahoo.co.id>
2019-03-19 07:16:30 +0700

    #13 - Add Ensemble Type Enum Model in hendisantika-api module

---
+++ hendisantika-api/src/main/java/com/hendisantika/hendisantikaapi/model/enums/EnsembleType.java
@@ -0,0 +1,15 @@
+package com.hendisantika.hendisantikaapi.model.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rest-api-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-03-19
+ * Time: 07:16
+ */
+public enum EnsembleType {
+    BAND,
+    ORCHESTRA
+}
