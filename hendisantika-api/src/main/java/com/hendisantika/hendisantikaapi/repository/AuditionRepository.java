c78f76d5c73088b18691bc6d578df4fa0497e7a2
hendisantika <hendisantika@yahoo.co.id>
2019-03-20 21:09:48 +0700

    #19 - Add Audition Repository in hendisantika-api module

---
+++ hendisantika-api/src/main/java/com/hendisantika/hendisantikaapi/repository/AuditionRepository.java
@@ -0,0 +1,23 @@
+package com.hendisantika.hendisantikaapi.repository;
+
+import com.hendisantika.hendisantikaapi.model.Audition;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rest-api-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-03-20
+ * Time: 21:09
+ */
+@Repository
+public interface AuditionRepository extends PagingAndSortingRepository<Audition, Long> {
+
+    Page<Audition> findAll(Pageable pageable);
+
+}
