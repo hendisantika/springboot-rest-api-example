5ce39d63bb3c6336c8840e402a1eac6121156421
hendisantika <hendisantika@yahoo.co.id>
2019-03-21 06:10:38 +0700

    #26 - Add Modify Main App in hendisantika-api module

--- hendisantika-api/src/main/java/com/hendisantika/hendisantikaapi/HendisantikaApiApplication.java
+++ hendisantika-api/src/main/java/com/hendisantika/hendisantikaapi/HendisantikaApiApplication.java
@@ -2,8 +2,11 @@ package com.hendisantika.hendisantikaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {HendisantikaApiApplication.class, Jsr310JpaConverters.class})
 public class HendisantikaApiApplication {
 
     public static void main(String[] args) {
