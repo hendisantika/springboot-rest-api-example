4101809cd590b9a1326b7bb44a0e03e44bb9359a
hendisantika <hendisantika@yahoo.co.id>
2019-03-22 06:06:08 +0700

    #31 - Add WebSecurityConfig in hendisantika-api module

---
+++ hendisantika-api/src/main/java/com/hendisantika/hendisantikaapi/config/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.hendisantika.hendisantikaapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rest-api-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-03-22
+ * Time: 06:05
+ */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security",
+                "/swagger-ui.html", "/webjars/**");
+    }
+
+}
