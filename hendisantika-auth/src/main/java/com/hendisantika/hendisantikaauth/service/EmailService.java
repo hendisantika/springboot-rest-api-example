f89e0688f23996902f5162a08aed13b1c6e7deb1
hendisantika <hendisantika@yahoo.co.id>
2019-03-24 05:48:11 +0700

    #53 - Add Email Service in hendisantika-api module

---
+++ hendisantika-auth/src/main/java/com/hendisantika/hendisantikaauth/service/EmailService.java
@@ -0,0 +1,30 @@
+package com.hendisantika.hendisantikaauth.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rest-api-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-03-24
+ * Time: 05:47
+ */
+@Service
+public class EmailService {
+
+    private JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Async
+    public void sendEmail(SimpleMailMessage email) {
+        mailSender.send(email);
+    }
+}
