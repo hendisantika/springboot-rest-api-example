2b3fd426dc1d45957c4a152ebe301c6c677357f5
hendisantika <hendisantika@yahoo.co.id>
2019-03-23 18:57:23 +0700

    #40 - Add new module in hendisantika-parent

---
+++ hendisantika-auth/src/main/java/com/hendisantika/hendisantikaauth/HendisantikaAuthApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.hendisantikaauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HendisantikaAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HendisantikaAuthApplication.class, args);
+    }
+
+}
