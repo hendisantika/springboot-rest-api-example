f9ff9b149b8116fd4c0a34bce7aae3a49536fec9
hendisantika <hendisantika@yahoo.co.id>
2019-03-25 06:41:05 +0700

    #62 - Add ProfileController in hendisantika-api module

---
+++ hendisantika-auth/src/main/java/com/hendisantika/hendisantikaauth/controller/ProfileController.java
@@ -0,0 +1,23 @@
+package com.hendisantika.hendisantikaauth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rest-api-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-03-25
+ * Time: 06:40
+ */
+@Controller
+public class ProfileController {
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "profile";
+    }
+
+}
